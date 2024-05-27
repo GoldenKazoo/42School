dc0c35c2fb98daf60b72aefb5e2c2c02af2fc527
Zakarya Chagar <zchagar@bess-f2r1s13.clusters.42paris.fr>
2024-05-27 19:14:29 +0200

    all files added not compiled

--- Libft/ft_strchr.c
+++ Libft/ft_strchr.c
@@ -1,27 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zchagar <zchagar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/27 12:55:31 by zchagar           #+#    #+#             */
-/*   Updated: 2024/05/27 18:15:23 by zchagar          ###   ########.fr       */
+/*   Created: 2024/05/27 17:48:30 by zchagar           #+#    #+#             */
+/*   Updated: 2024/05/27 18:42:15 by zchagar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <stdlib.h>
-/*void ft_bzero(void *pointer, size_t n)
+char	*ft_strchr(char *str, int searchedChar)
 {
 	int	i;
-	int	*p;
+	char	*p;
 
 	i = 0;
-	*p = pointer;
-	while (i <= n)
+	p = 0;
+	while (str[i] != '\0')
 	{
-		p[i] = 0;
+		if (str[i] == searchedChar)
+		{
+			p = (char *) &str[i];
+			return (p);
+		}
 		i++;
 	}
+	if (str[i] == searchedChar)
+		{
+			p = (char *) &str[i];
+			return (p);
+		}
+	return (p);
 }
-*/
+
+int	main()
+{
+	printf("%s", ft_strchr("teste",'e'));
+}
+
